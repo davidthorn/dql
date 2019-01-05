76343455250e13843f0e5944162b133f29857dca
David Thorn <david.thorn221278@googlemail.com>
2019-01-05 06:57:39 +0100

    saved

--- endpoints/home.ts
+++ endpoints/home.ts
@@ -2,7 +2,10 @@ import { BodyDQLEndpoint } from '../src/body/BodyDQLEndpoint';
 
 const endpoint: BodyDQLEndpoint = {
     body : {
-        
+        name: {
+            type: 'string',
+            required: true
+        }
     },
     middleware: (req, res) => {
         res.status(200).send({
