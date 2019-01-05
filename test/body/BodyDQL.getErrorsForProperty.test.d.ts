3bc4b620d669b516be8d1f7ffe839aaa5ff08930
David Thorn <david.thorn221278@googlemail.com>
2019-01-05 07:17:23 +0100

    removed all .d.ts files as they will be generated on a build

--- test/body/BodyDQL.getErrorsForProperty.test.d.ts
+++
@@ -1,4 +0,0 @@
-import { BodyDQL } from "../../src/body/BodyDQL";
-export declare class BodyDQLUnitTest extends BodyDQL {
-    "returns array of errors for property name when errors property exists"(): void;
-}
